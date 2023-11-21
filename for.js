f5c160d46fc490d7d3e9462f2717f9706b0a6804
KIM YONGHWAN <yonghwane135@gmail.com>
2023-11-21 23:31:36 +0900

    learn event bubbling

--- for.js
+++ for.js
@@ -1,10 +1,10 @@
-var button = $(".tab-button");
-
-$(".tab-button")
-	.eq(0)
-	.on("click", function () {
-		$(".tab-button").removeClass("orange");
-		$(".tab-button").eq(0).addClass("orange");
-		$(".tab-content").hide();
-		$(".tab-content").eq(0).show();
-	});
+for (let i = 0; i < 3; i++) {
+	$(".tab-button")
+		.eq(i)
+		.on("click", function () {
+			$(".tab-button").removeClass("orange");
+			$(".tab-button").eq(i).addClass("orange");
+			$(".tab-content").hide();
+			$(".tab-content").eq(i).show();
+		});
+}
